074f2d919acab421dddd6837c313ae3cafc8f460
Facundo <facundoserradev@gmail.com>
2025-04-12 22:57:30 -0300

    [Parte 4.6] update a las views, las volvemos genericas

--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.views import generic
 from django.urls import reverse
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
@@ -7,21 +8,23 @@ from django.db.models import F
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("app/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = "app/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "app/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "app/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "app/results.html"
 
 
 def vote(request, question_id):
@@ -41,7 +44,3 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("app:results", args=(question.id,)))
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "app/detail.html", {"question": question})

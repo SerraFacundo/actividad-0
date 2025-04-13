9aedde79e3b4027a53f1a6b448f98a10ecaca372
Facundo <facundoserradev@gmail.com>
2025-04-12 21:40:12 -0300

    [Parte 3.5] Se agrega una view encargada de mostrar un error 404 en caso de no encontrar el objeto

--- project/app/views.py
+++ project/app/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
+    template = loader.get_template("app/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
@@ -23,3 +24,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "app/detail.html", {"question": question})
